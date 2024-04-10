0de377340146149b13ef79ef09e009abe77d5636
maksym <maksym.polyakov@yahoo.ca>
2024-04-10 17:38:44 -0500

    Pair work

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -1,23 +1,23 @@
 const chai = require('chai');
 const assert = chai.assert;
 
-const wordSearch = require('../wordsearch.js')
+const wordSearch = require('../wordsearch.js');
 
 describe("#wordSearch()", function() {
   it("should return false if the word is not present", function() {
     const result = wordSearch([
       ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
       ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
-      ['Y', 'F', 'C', 'F', 'Q', 'U', 'A', 'L'],
-      ['H', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
-      ['W', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
-      ['B', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
-      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['Y', 'F', 'A', 'A', 'N', 'K', 'A', 'L'],
+      ['H', 'R', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['W', 'A', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['B', 'N', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['U', 'K', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK');
 
-    assert.isFalse(result);
+    assert.isTrue(result);
   });
 
   it("should return true if the word is present", function() {
@@ -31,7 +31,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD');
 
     assert.isTrue(result);
   });
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    console.log(horizontalJoin)
+    for (let l of horizontalJoin) {
+            if (l.includes(word)) return true
     }
-}
 
-module.exports = wordSearch
+    newArray = []
+    for (let i = 0; i < letters[0].length; i++) {
+      newArray.push([])
+      for (let j = 0; j < letters.length; j++) {
+        newArray[i].push(letters[j][i])
+      }
+      combinedLetters = newArray[i].join('')
+      console.log(combinedLetters)
+      if (combinedLetters.includes(word)) return true
+    }
+    return false
+  }
+module.exports = wordSearch;
